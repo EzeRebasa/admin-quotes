ec3d3264ffb0f5367ba0f94e06e4004a84a20be9
Ezequiel <ezequielrebasa@gmail.com>
2020-05-02 15:28:13 -0300

    Component Quote and local storage added

---
+++ quotes/src/components/Quote.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Quote = ({ quote, deleteQuote }) => (
+  <div className="quote">
+    <p>
+      Pet: <span>{quote.pet}</span>
+    </p>
+    <p>
+      Owner: <span>{quote.owner}</span>
+    </p>
+    <p>
+      Date: <span>{quote.date}</span>
+    </p>
+    <p>
+      Time: <span>{quote.time}</span>
+    </p>
+    <p>
+      Symptoms: <span>{quote.symptoms}</span>
+    </p>
+    <button
+        className="button delete u-full-width"
+        onClick={ () => deleteQuote(quote.id) }
+    > Delete &times;</button>
+  </div>
+);
+
+export default Quote;
